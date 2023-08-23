7630dbd7cdfbbf117134a2bc2b263847917551b0
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-08-23 18:24:53 -0300

    3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -2,3 +2,7 @@ select titulo from livros;
 
 select nome from autores
 where nascimento < '1900-01-01';
+
+select livros.titulo, autores.nome
+from livros
+inner join autores on autores.id = 1 and livros.autor_id = autores.id;
