b7f9fae810f8cc252eb4ac855b9ad14ca5bda6a9
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-09-06 17:43:49 -0300

    Ex. 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,8 +1,12 @@
-select titulo from livros;
+1) select titulo from livros;
 
-select nome from autores
+2) select nome from autores
 where nascimento < '1900-01-01';
 
-select livros.titulo, autores.nome
+3) select livros.titulo, autores.nome
 from livros
 inner join autores on autores.id = 1 and livros.autor_id = autores.id;
+
+4) select alunos.nome
+from alunos
+inner join matriculas on matriculas.curso = 'Engenharia de Software' and matriculas.aluno_id = alunos.id;
