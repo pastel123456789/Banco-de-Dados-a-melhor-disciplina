f1200637591162e7b8c717e52a9018754e98c5f9
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-09-20 18:16:39 -0300

    ex2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,6 +4,15 @@ BEGIN
 SELECT * FROM autor;
 END
 
+ex 2 
+CREATE PROCEDURE sp_LivrosPorCategoria(IN nome_categoria varchar(200))
+BEGIN
+select Livro.Titulo, Categoria.Nome as categoria
+from Livro
+inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
+where nome_categoria = Categoria.Nome;
+END;
+
 
 
 
