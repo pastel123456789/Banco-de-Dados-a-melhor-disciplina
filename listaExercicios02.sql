4b17ee316fa60e4bea37ea49b661eab0618ad649
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-09-22 20:01:09 -0300

    correção ex2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,10 +4,9 @@ BEGIN
 SELECT * FROM autor;
 END
 
-ex 2 
 CREATE PROCEDURE sp_LivrosPorCategoria(IN nome_categoria varchar(200))
 BEGIN
-select Livro.Titulo, Categoria.Nome as categoria
+select *
 from Livro
 inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
 where nome_categoria = Categoria.Nome;
