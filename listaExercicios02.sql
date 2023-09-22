5e8c223fd74e0946c2608d7d7e01013e23be88c7
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-09-22 20:01:43 -0300

    Ex 6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -30,6 +30,15 @@ where Ano_Publicacao <= ano
 ORDER BY Ano_Publicacao desc;
 END;
 
+ex 6
+CREATE PROCEDURE sp_TitulosPorCategoria(IN nome_categoria varchar(200))
+BEGIN
+select Livro.Titulo, Categoria.Nome as categoria
+from Livro
+inner join Categoria on Categoria.Categoria_ID = Livro.Categoria_ID
+where nome_categoria = Categoria.Nome;
+END;
+
 
 
 
