01aa90c8b561b018d3adc08498cfbcd82b8e0be1
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-10-09 18:17:25 -0300

    ex1

--- Exercicios4Bm/exerciciosDeFixacao20231002.sql
+++ Exercicios4Bm/exerciciosDeFixacao20231002.sql
@@ -1,1 +1,25 @@
+-- 1 
+DROP TABLE nomes;
+CREATE TABLE nomes (
+nome VARCHAR(100)
+);
 
+INSERT INTO nomes VALUES
+('Roberta'),
+('Roberto'),
+('Maria Clara'),
+('João');
+
+select * from nomes;
+
+select upper(nome) from nomes;
+select length(nome) from nomes;
+select concat('Sr. ', nome) from nomes where nome like '%o';
+select concat('Sra. ', nome) from nomes where nome like '%a';
+
+DROP TABLE produtos;
+CREATE TABLE produtos (
+produto VARCHAR(100),
+preco FLOAT,
+quantidade INT
+);
