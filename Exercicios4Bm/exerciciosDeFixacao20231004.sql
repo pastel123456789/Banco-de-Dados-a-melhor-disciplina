2e273096ffcb5b0147a99995c634737549fbbe2d
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-10-14 13:52:23 -0300

    ex2

--- Exercicios4Bm/exerciciosDeFixacao20231004.sql
+++ Exercicios4Bm/exerciciosDeFixacao20231004.sql
@@ -19,3 +19,39 @@ BEGIN
 CLOSE c_livros_genero;
 RETURN total;
 END
+
+-- 2
+CREATE FUNCTION listar_livros_por_autor(
+    autor_nome VARCHAR(100),
+    autor_sobrenome VARCHAR(100)
+) RETURNS VARCHAR(1000) DETERMINISTIC
+BEGIN
+    DECLARE fim INT DEFAULT 0;
+    DECLARE titulo_livro VARCHAR(200);
+    DECLARE c_nome CURSOR FOR 
+        SELECT Livro.titulo
+        FROM Livro_Autor
+        INNER JOIN Livro ON Livro_Autor.id_livro = Livro.id
+        INNER JOIN Autor ON Livro_Autor.id_autor = Autor.id
+        WHERE Autor.primeiro_nome = autor_nome AND Autor.ultimo_nome = autor_sobrenome;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fim = 1;
+
+    SET SESSION group_concat_max_len = 1000000;
+
+    OPEN c_nome;
+
+    SET @titulos = '';
+
+    read_loop: LOOP
+        FETCH c_nome INTO titulo_livro;
+        IF fim THEN
+            LEAVE read_loop;
+        END IF;
+        SET @titulos = CONCAT_WS('\n', @titulos, titulo_livro);
+    END LOOP;
+
+    CLOSE c_nome;
+
+    RETURN @titulos;
+END
