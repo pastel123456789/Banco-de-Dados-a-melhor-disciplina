51f8549a6c41e6e4ab8b8964fe81cf6f57ed5c38
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-10-25 18:15:52 -0300

    2

--- Exercicios4Bm/exerciciosDeFixacao20231025.sql
+++ Exercicios4Bm/exerciciosDeFixacao20231025.sql
@@ -3,3 +3,9 @@ CREATE TRIGGER inserir_cliente BEFORE INSERT ON Clientes
 FOR EACH ROW
 INSERT INTO Auditoria(mensagem, data_hora) VALUES
 ('cliente inserido', NOW());
+
+-- 2
+CREATE TRIGGER deletar_cliente BEFORE DELETE ON Clientes 
+FOR EACH ROW
+INSERT INTO Auditoria(mensagem, data_hora) VALUES
+('tentativa de excluir cliente', NOW());
