f313965b810efdec32809e6e87e62ca971a80a80
pastel123456789 <113729257+pastel123456789@users.noreply.github.com>
2023-10-25 20:14:30 -0300

    4

--- Exercicios4Bm/exerciciosDeFixacao20231025.sql
+++ Exercicios4Bm/exerciciosDeFixacao20231025.sql
@@ -15,3 +15,16 @@ CREATE TRIGGER atualizar_cliente AFTER UPDATE ON Clientes
 FOR EACH ROW
 INSERT INTO Auditoria(mensagem, data_hora) VALUES
 (CONCAT('Antigo nome: ', OLD.nome, '. Novo nome: ', NEW.nome), NOW());
+
+-- 4 não descobri como faz pra impedir o update e fazer um registro na auditoria ao mesmo tempo, só consigo fazer ou um ou outro
+CREATE TRIGGER cliente_nulo AFTER UPDATE ON Clientes 
+FOR EACH ROW
+BEGIN
+IF NEW.nome = '' THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Nome inválido';
+INSERT INTO Auditoria(mensagem, data_hora) VALUES
+('Nome inválido', NOW());
+DELETE FROM Clientes WHERE nome = NULL;
+END IF;
+END;
